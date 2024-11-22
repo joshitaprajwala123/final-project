021c34da605eefbaf4b121c8f98dc9c4d109e9e1
joshitaprajwala123 <jhansi.mr@gmail.com>
2024-11-22 17:16:41 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,10 @@
+while True:
+    act_username="joshitaprajwala123"
+    act_password="joshita_praju123"
+    username=input("username: ")
+    password=input("password: ")
+    if(username==act_username)and(password==act_password):
+        print ("logged in sucessfully")
+        break
+    else:
+        print("sorry,its wrong")
